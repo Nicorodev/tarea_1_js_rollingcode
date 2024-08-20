95d2c030c3a3a74991ee2989d216dbd5399f1499
Nicorodev <nicorodev@gmail.com>
2024-08-20 20:35:44 -0300

    Primera tarea realizada

---
+++ app.js
@@ -0,0 +1,32 @@
+//Tarea
+// Crear dos variables de tipo number
+// mostrar por consola los resultados de las siguientes operaciones:
+// Sumar las dos variables
+// Restar las dos variables
+// Multiplicar las dos variables
+// Dividir ambas variables
+// comparar si la primera variable es mayor o igual que la segunda
+// Comparar si la primera variable es distinta de la segunda
+
+let numero1 = 8;
+let numero2 = 4;
+
+
+// Suma
+console.log(numero1 + numero2);
+
+// Resta
+console.log(numero1 - numero2);
+
+// Multiplicación
+console.log(numero1 * numero2);
+
+// División
+console.log(numero1 / numero2);
+
+// Mayor o igual
+console.log(numero1 >= numero2);
+
+// Comparación 
+console.log(numero1 != numero2);
+
